c2d77badc05ce04948a78539f95eea82d99eac74
TYH71 <yuhoetan@gmail.com>
2022-07-18 03:10:49 +0800

    refactor: image display

--- main.py
+++ main.py
@@ -183,10 +183,10 @@ if __name__ == '__main__':
         # positive explanations
         with col2:
             pos_img_boundary = generate_img_boundary(explanation, positive=True, max_features=max_features, hide_rest=hide_rest)
-            st.image(pos_img_boundary, caption="Positive Explanation; Predicted: {}".format(classes[explanation.top_labels[0]]))
+            st.image(pos_img_boundary, caption="Positive Explanation; Predicted: {}".format(classes[explanation.top_labels[0]]), use_column_width=True)
             
         # negative explanations
         with col3:
             neg_img_boundary = generate_img_boundary(explanation, positive=False, max_features=max_features, hide_rest=hide_rest)
-            st.image(neg_img_boundary, caption="Negative Explanation; Predicted: {}".format(classes[explanation.top_labels[0]]))
+            st.image(neg_img_boundary, caption="Negative Explanation; Predicted: {}".format(classes[explanation.top_labels[0]]), use_column_width=True)
       
